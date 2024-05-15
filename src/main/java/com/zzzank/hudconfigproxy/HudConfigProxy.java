b8d7529178a9d48334fd169d70b569b5d67baad9
ZZZank <47418975+ZZZank@users.noreply.github.com>
2024-05-15 02:32:11 +0000

    use event subsriber instead of event handler

--- src/main/java/com/zzzank/hudconfigproxy/HudConfigProxy.java
+++ src/main/java/com/zzzank/hudconfigproxy/HudConfigProxy.java
@@ -1,25 +1,31 @@
 package com.zzzank.hudconfigproxy;
 
 import net.minecraftforge.client.GuiIngameForge;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-/**
- * HudConfigProxy
- */
 @Mod(modid = HudConfigProxy.MOD_ID, name = HudConfigProxy.MOD_NAME, version = HudConfigProxy.VERSION)
-public class HudConfigProxy {
+public final class HudConfigProxy {
     public static final String MOD_ID = "hudconfigproxy";
     public static final String MOD_NAME = "HUD Config Proxy";
     public static final String VERSION = "1.0.0";
 
-    // public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+    // public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
-    @Mod.EventHandler
-    public void syncWithProxy(ConfigChangedEvent event) {
-        if (event.getModID() != MOD_ID) {
+    public HudConfigProxy() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    @SubscribeEvent
+    public void syncWithProxy(OnConfigChangedEvent event) {
+        if (!event.getModID().equals(HudConfigProxy.MOD_ID)) {
             return;
         }
+        ConfigManager.sync(MOD_ID, Config.Type.INSTANCE);
         GuiIngameForge.renderVignette    = ConfigProxy.renderVignette   ;
         GuiIngameForge.renderHelmet      = ConfigProxy.renderHelmet     ;
         GuiIngameForge.renderPortal      = ConfigProxy.renderPortal     ;
